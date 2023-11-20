d25ad431ec2dc37fb5eccfcad2befa6e4d7b5b8e
barbsuk20 <146033546+barbsuk20@users.noreply.github.com>
2023-11-20 23:15:17 +0000

    edited html and added basic JS

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function searchSumit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchElement = document.querySelector("#search-form");
+searchElement.addEventListener("submit", searchSumit);
