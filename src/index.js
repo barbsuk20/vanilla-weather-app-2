d0d025a7eb56ed0136abba4b3138783c2e6bea8c
barbsuk20 <146033546+barbsuk20@users.noreply.github.com>
2023-11-20 23:42:17 +0000

    added axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+function searchCity(city) {
+  let apiKey = `31f697f233da06aa04dbt2b68de8ob94`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
+}
+
 function searchSumit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
